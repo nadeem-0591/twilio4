3e637d25c70392d91fce58ae535006b4ab6a1667
Nadeem <shaiknadeem0591@gmail.com>
2023-07-08 10:44:03 +0530

    fixed defualt time

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
-import pywhatkit as kit
+import time
+import pyautogui
 
 app = Flask(__name__)
 
@@ -8,16 +9,22 @@ def index():
     if request.method == 'POST':
         group_id = request.form['group_id']
         message = request.form['message']
-        hour = int(request.form['hour'])
-        minute = int(request.form['minute'])
 
         try:
-            kit.sendwhatmsg_to_group(group_id, message, hour, minute)
+            send_whatsapp_message(group_id, message)
             return "WhatsApp message sent successfully."
         except Exception as e:
             return f"An exception occurred while sending the WhatsApp message: {str(e)}"
 
     return render_template('index.html')
+
+def send_whatsapp_message(group_id, message):
+    time.sleep(5)  
+    pyautogui.hotkey('ctrl', 't')  
+    pyautogui.typewrite(f'https://web.whatsapp.com/accept?code={group_id}\n')  
+    time.sleep(10)  
+    pyautogui.typewrite(message) 
+    pyautogui.press('enter')  
 
 if __name__ == '__main__':
     app.run()
